fe42e8c791519aa61da3ac63828c1763e1520c35
amolwalia <awalia1656@gmail.com>
2025-10-16 14:27:37 -0700

    NativeWind added

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-lg font-bold text-gray-900">Home Tab</Text>
+    </View>
+  );
+}
